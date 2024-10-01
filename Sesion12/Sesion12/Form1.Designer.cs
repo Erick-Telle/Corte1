bed9669981e6cbdb63313a554e40fb049ee206ea
Erick Tellez <ebguido@uamv.edu.ni>
2024-09-30 19:12:45 -0600

    Primer commit tarde pero seguro

---
+++ Sesion12/Sesion12/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Sesion12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbCodigo = new System.Windows.Forms.TextBox();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbDescripcion = new System.Windows.Forms.TextBox();
+            this.tbPrecio = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.gbProducto = new System.Windows.Forms.GroupBox();
+            this.dgbProduct = new System.Windows.Forms.DataGridView();
+            this.gbProducto.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgbProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Descripcion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(74, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Precio";
+            // 
+            // tbCodigo
+            // 
+            this.tbCodigo.Location = new System.Drawing.Point(136, 44);
+            this.tbCodigo.Name = "tbCodigo";
+            this.tbCodigo.Size = new System.Drawing.Size(145, 20);
+            this.tbCodigo.TabIndex = 4;
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(136, 89);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(145, 20);
+            this.tbNombre.TabIndex = 5;
+            // 
+            // tbDescripcion
+            // 
+            this.tbDescripcion.Location = new System.Drawing.Point(136, 123);
+            this.tbDescripcion.Name = "tbDescripcion";
+            this.tbDescripcion.Size = new System.Drawing.Size(145, 20);
+            this.tbDescripcion.TabIndex = 6;
+            // 
+            // tbPrecio
+            // 
+            this.tbPrecio.Location = new System.Drawing.Point(136, 165);
+            this.tbPrecio.Name = "tbPrecio";
+            this.tbPrecio.Size = new System.Drawing.Size(145, 20);
+            this.tbPrecio.TabIndex = 7;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(301, 135);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(106, 50);
+            this.btnAgregar.TabIndex = 8;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // gbProducto
+            // 
+            this.gbProducto.Controls.Add(this.dgbProduct);
+            this.gbProducto.Location = new System.Drawing.Point(51, 201);
+            this.gbProducto.Name = "gbProducto";
+            this.gbProducto.Size = new System.Drawing.Size(600, 171);
+            this.gbProducto.TabIndex = 9;
+            this.gbProducto.TabStop = false;
+            this.gbProducto.Text = "groupBox1";
+            // 
+            // dgbProduct
+            // 
+            this.dgbProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgbProduct.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgbProduct.Location = new System.Drawing.Point(3, 16);
+            this.dgbProduct.Name = "dgbProduct";
+            this.dgbProduct.Size = new System.Drawing.Size(594, 152);
+            this.dgbProduct.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gbProducto);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.tbPrecio);
+            this.Controls.Add(this.tbDescripcion);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.tbCodigo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gbProducto.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgbProduct)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbCodigo;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbDescripcion;
+        private System.Windows.Forms.TextBox tbPrecio;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.GroupBox gbProducto;
+        private System.Windows.Forms.DataGridView dgbProduct;
+    }
+}
+
---
+++ Sesion12/Sesion12/Form1.cs
@@ -0,0 +1,48 @@
+﻿using Sesion12.models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sesion12
+{
+    public partial class Form1 : Form
+    {
+        List<Product> products;
+        public Form1()
+        {
+            InitializeComponent();
+            products = new List<Product>();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Product product = new Product();
+            product.Id = int.Parse(tbCodigo.Text);
+            product.Name = tbNombre.Text;
+            product.Description = tbDescripcion.Text;
+            product.Price = int.Parse(tbPrecio.Text);
+            products.Add(product);
+            showDate();
+            clearText();
+        }
+        private void showDate()
+        {
+            dgbProduct.DataSource = null;
+            dgbProduct.DataSource = products;
+        }
+        private void clearText()
+        {
+            tbCodigo.Clear();
+            tbNombre.Clear();
+            tbDescripcion.Clear();
+            tbPrecio.Clear();
+            tbCodigo.Focus();
+        }
+    }
+}
